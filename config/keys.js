28ee0ad42257772bb0fdb021d3c740e0d10b0b60
Andrew <andrew.git11@gmail.com>
2021-02-14 13:58:42 +0000

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://andrew-mongodb-experimenal:jFz-nfnU4Q-3Ya@cluster0.xcreo.mongodb.net/experimental-db?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
