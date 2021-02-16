7f43a9f5fb613f7f77af3a92e9e885b1729c6cb4
Andrew <andrew.git11@gmail.com>
2021-02-16 00:14:07 +0000

    minor change to errors wording

--- validation/login.js
+++ validation/login.js
@@ -12,7 +12,7 @@ module.exports = function validateLoginInput(data) {
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid (please mind the case, too)";
+    errors.email = "Email is invalid";
   }
 
 // Password checks
